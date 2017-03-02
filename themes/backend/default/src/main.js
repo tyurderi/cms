8aaa34f0ef9a460ff149b1e50eb89d7790518ae7
Tommy <tyurderi@yahoo.de>
2017-03-02 20:34:13 +0100

    Implement basic vuex store

--- themes/backend/default/src/main.js
+++ themes/backend/default/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App'
+
 import router from './router'
+import store from './store';
+
 import Http from 'vue-resource';
 
 Vue.use(Http);
@@ -8,6 +11,7 @@ Vue.use(Http);
 new Vue({
     el: '#app',
     router,
+    store,
     template: '<App/>',
     components: { App },
     http: {
