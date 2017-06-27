17fdcf71de452dd12207220b04e993b707c1db52
tm-rm <meier@rhinos-media.de>
2017-06-27 15:30:55 +0200

    Code formatting

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -1,17 +1,17 @@
-var utils = require('./utils')
-var config = require('../config')
-var isProduction = process.env.NODE_ENV === 'production'
+let utils = require('./utils'),
+    config = require('../config'),
+    isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  loaders: utils.cssLoaders({
-    sourceMap: isProduction
-      ? config.build.productionSourceMap
-      : config.dev.cssSourceMap,
-    extract: isProduction
-  }),
-  postcss: [
-    require('autoprefixer')({
-      browsers: ['last 2 versions']
-    })
-  ]
+    loaders: utils.cssLoaders({
+        sourceMap: isProduction
+            ? config.build.productionSourceMap
+            : config.dev.cssSourceMap,
+        extract: isProduction
+    }),
+    postcss: [
+        require('autoprefixer')({
+            browsers: ['last 2 versions']
+        })
+    ]
 }
