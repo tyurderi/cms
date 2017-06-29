8652bccb9b32fb042a864114ccab78501bc88df1
tm-rm <meier@rhinos-media.de>
2017-06-29 12:28:49 +0200

    Select site in modal instead of menu/sidebar

--- ext/system/BackendMenu/Views/backend/src/components/Sidebar.vue
+++ ext/system/BackendMenu/Views/backend/src/components/Sidebar.vue
@@ -1,22 +1,42 @@
 <template>
     <div class="sidebar">
         <div class="sidebar-header">
-            <div class="site-list">
-                <div class="site" v-for="(site, key) in sites" :key="key"
-                     v-if="site.id == selectedSite || selectingSite"
-                     @click="selectSite(site)">
-                    <div class="site-title">
-                        {{site.label}}
-                    </div>
-                    <div class="site-host">
-                        {{site.host}}
-                    </div>
+            <div class="site" v-if="site" @click="selectingSite = true">
+                <div class="site-title">
+                    {{site.label}}
+                </div>
+                <div class="site-host">
+                    {{site.host}}
                 </div>
             </div>
         </div>
-        <div class="menu" v-if="!selectingSite">
+        <div class="menu">
             <sidebar-menu ref="menu"></sidebar-menu>
         </div>
+        <modal mode="fixed" width="500px" height="300px" v-if="selectingSite">
+            <div class="site-selector" slot="content">
+                <div class="selector-header">
+                    <div class="header-title">
+                         Select site
+                    </div>
+                    <div class="header-close" @click="selectingSite = false">
+                        <i class="fa fa-times"></i>
+                    </div>
+                </div>
+                <div class="selector-sites">
+                    <div class="site" v-for="(site, key) in sites"
+                         :class="{ active: parseInt(site.id) === selectedSite }"
+                         @click="selectedSite = parseInt(site.id)">
+                        <div class="site-title">
+                            {{site.label}}
+                        </div>
+                        <div class="site-host">
+                            {{site.host}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </modal>
     </div>
 </template>
 
@@ -35,6 +55,10 @@ export default {
         sites()
         {
             return this.$store.getters['site/items']
+        },
+        site()
+        {
+            return this.sites.find(site => parseInt(site.id) === this.selectedSite);
         }
     },
     methods: {
@@ -56,3 +80,58 @@ export default {
     }
 }
 </script>
+
+<style lang="less">
+.site-selector {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    .selector-header {
+        display: flex;
+        flex-direction: row;
+        border-bottom: 1px solid #ddd;
+        .header-title {
+            flex: 1;
+            padding: 15px;
+            font-weight: bold;
+        }
+        .header-close {
+            width: 40px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 20px;
+            &:hover {
+                background: rgba(0, 0, 0, 0.05);
+                cursor: pointer;
+            }
+        }
+    }
+    .selector-sites {
+        flex: 1;
+        overflow-y: auto;
+        .site {
+            padding: 15px;
+            border-bottom: 1px dashed #ddd;
+            &.active {
+                .site-title {
+                    font-weight: bold;
+                }
+            }
+            &:last-child {
+                border-bottom: 0 none;
+            }
+            &:hover {
+                background: rgba(0, 0, 0, 0.05);
+                cursor: pointer;
+            }
+            .site-title {
+            
+            }
+            .site-label {
+            
+            }
+        }
+    }
+}
+</style>
